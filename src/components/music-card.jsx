d668f0db5b553ed4ae4fddd8529d53c32f9be7bf
biohackerellie <epkerns@gmail.com>
2023-04-26 00:04:57 +0000

    setup redux

--- src/components/music-card.jsx
+++ src/components/music-card.jsx
@@ -1,14 +1,29 @@
 import { BsPlayCircleFill } from "react-icons/bs";
+import React from 'react'
+import { useDispatch } from "react-redux";
+import { setCurrentSong, togglePlaying } from "../redux/store";
 
 
-const MusicCard = (props) => {
+
+const MusicCard = ({ song }) => {
+	const dispatch = useDispatch();
+
+	const handlePlay = () => {
+		dispatch(setCurrentSong(song));
+		dispatch(togglePlaying());
+	}
+
 	return(
 		<div className="bg-gray-900 shadow-xl rounded p-3">
 			<div className="group relative">
-				<img className="w-full md:w-72 block rounded" src={props.image} alt="" />
+				<img className="w-full md:w-72 block rounded" src={song.image} alt="" />
 				<div className="absolute bg-black rounded bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 flex items-center group-hover:opacity-100 transition justify-evenly">
-					<button className="hover:scale-110 text-white opacity-0 transform translate-y-3 group-hover:translate-y-0 group-hover:opacity-100 transition">
-						<BsPlayCircleFill />
+					<button 
+						className="hover:scale-110 text-white opacity-0 transform translate-y-3 
+							group-hover:translate-y-0 group-hover:opacity-100 transition"
+						onClick={handlePlay}
+							>
+						<BsPlayCircleFill scale={50} />
 					</button>
 				</div>
 			</div>
