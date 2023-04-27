072c2a69277444726b8f4fe38bab76a60ec592e4
biohackerellie <epkerns@gmail.com>
2023-04-27 08:54:24 -0600

    fixed alignment of music player item

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -47,33 +47,41 @@ const Player = () => {
   };
  
 
-  return (
-    <div className="fixed bottom-0 left-0 right-0 bg-slate-300 bg-opacity-80 backdrop-blur-sm h-[150px]" >
-      <div className="flex justify-around bg-transparent drop-shadow-md  items-center h-full ">
+	return (
+    <div className="fixed bottom-0 left-0 right-0 bg-gradient-to-br from-stone-100 via-gray-400 to-stone-400 bg-opacity-50 backdrop-blur-sm h-[100px] flex items-center justify-center">
+      <div className="flex w-full max-w-screen-xl p4 md:py-8 mx-auto justify-between bg-transparent drop-shadow-md items-center h-full px-8">
         {currentSong && (
           <>
-            
-            <img src={`/api/songs/${currentSong.image}`} alt={currentSong.title} className="w-16 h-16" />
-            <div className="bg-transparent">
-              <h4 className="bg-transparent">{currentSong.title}</h4>
-              <p className="bg-transparent">{currentSong.artist}</p>
+            <div className="flex items-center space-x-4 bg-transparent"> 
+              <img src={`/api/songs/${currentSong.image}`} alt={currentSong.title} className="w-16 h-16" />
+              <div className="bg-transparent">
+                <h4 className="bg-transparent">{currentSong.title}</h4>
+                <p className="bg-transparent">{currentSong.artist}</p>
+              </div>
             </div>
-            
-            <button onClick={handlePlayPause}>
-              {isPlaying ? (
-                <BsPauseCircleFill className="bg-transparent border-none outline-none" size={75} />
-              ) : (
-                <BsPlayCircleFill className="bg-transparent fill-blue-500 border-none outline-none" size={75} />
-              )}
-            </button>
-            <input type="range" min="0" max={duration || 0} value={currentTime || 0} onChange={handleTimeChange} className="w-64 h-1 bg-blue-500 rounded-full" />
-            <input type="range" min="0" max="1" step="0.01" value={volume || 0} onChange={handleVolumeChange} className="w-24 h-1 bg-blue-500 rounded-full" />
+					
             <audio ref={audioRef} src={`/api/songs/${currentSong.audioFile}`} preload="auto" onLoadedMetadata={() => dispatch(setDuration(audioRef.current.duration))} />
           </>
         )}
+            <div className="flex flex-col items-center space-y-2 bg-transparent"> 
+              <button onClick={handlePlayPause} disabled={!currentSong}>
+                {isPlaying ? (
+                  <BsPauseCircleFill className="bg-transparent border-none outline-none" size={50} />
+                ) : (
+                  <BsPlayCircleFill className="bg-transparent fill-blue-500 border-none outline-none" size={50} />
+                )}
+              </button>
+              <input type="range" min="0" max={duration || 0} value={currentTime || 0} onChange={handleTimeChange} className="w-64 h-1 bg-blue-500 rounded-full" disabled={!currentSong} />
+            </div>
+
+            <div className="flex justify-end"> 
+              <input type="range" min="0" max="1" step="0.01" value={volume || 0} onChange={handleVolumeChange} className="w-24 h-1 bg-blue-500 rounded-full" />
+            </div>
+
       </div>
     </div>
   );
 }
 
 export default Player;
+
