815910c59f0e6e36ddf8b067f1930fe6fbae4f0b
biohackerellie <epkerns@gmail.com>
2023-04-27 13:03:02 -0600

    built album page

---
+++ src/components/album-songs.jsx
@@ -0,0 +1,4 @@
+import { BsPlayCircleFill, BsPauseCircleFill } from "react-icons/bs";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCurrentSong, togglePlaying } from "../redux/store";
