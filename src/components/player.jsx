1d7905ef909f662116e0812b93c29cfe53da6b55
biohackerellie <epkerns@gmail.com>
2023-04-27 02:33:21 +0000

    update player

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -49,14 +49,16 @@ const Player = () => {
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-slate-300 bg-opacity-80 backdrop-blur-sm h-[150px]" >
-      <div className="flex justify-evenly bg-transparent drop-shadow-md  items-center h-full ">
+      <div className="flex justify-around bg-transparent drop-shadow-md  items-center h-full ">
         {currentSong && (
           <>
+            
             <img src={`/api/songs/${currentSong.image}`} alt={currentSong.title} className="w-16 h-16" />
             <div className="bg-transparent">
               <h4 className="bg-transparent">{currentSong.title}</h4>
               <p className="bg-transparent">{currentSong.artist}</p>
             </div>
+            
             <button onClick={handlePlayPause}>
               {isPlaying ? (
                 <BsPauseCircleFill className="bg-transparent border-none outline-none" size={75} />
