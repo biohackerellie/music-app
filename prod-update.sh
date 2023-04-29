744725dc63b3d0ff38376700f1759e28bbaf659d
biohackerellie <epkerns@gmail.com>
2023-04-29 09:48:30 -0600

    update build

---
+++ prod-update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get the name of the service that has changes
+if git diff --quiet HEAD..HEAD~ -- ./server; then
+  service=server
+elif git diff --quiet HEAD..HEAD~ -- ./front-end; then
+  service=frontend
+else
+  service=all
+fi
+
+# Update the repository to the latest changes
+git fetch origin main
+git checkout main
+git pull origin main
+
+# Rebuild the Docker container for the specified service
+if [ "$service" == "all" ]; then
+  docker-compose build
+else
+  docker-compose build $service
+fi
+
+# Restart the Docker containers
+docker-compose down
+docker-compose up -d
