4f62ece0a802ec34880e26dd2c521f12ddb36742
biohackerellie <epkerns@gmail.com>
2023-07-24 11:32:13 -0600

    next

--- next/src/app/api/songs/[id]/route.ts
+++ next/src/app/api/songs/[id]/route.ts
@@ -2,11 +2,12 @@ import { NextResponse, NextRequest } from 'next/server';
 import { prisma } from '@/utils/prisma';
 
 export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: number } }
+	request: NextRequest,
+	{ params }: { params: { id: number } }
 ) {
-  const id = Number(params.id);
+	const id = Number(params.id);
 	const song = await prisma.songs.findUnique({
 		where: { id },
 	});
 	return NextResponse.json(song);
+}
